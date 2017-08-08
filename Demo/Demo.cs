0ec773ae7e6e787b017f0d080b68e243a40cf849
Rodja Trappe <rodja@perpetual-mobile.de>
2017-08-08 11:02:45 +0200

    added xamarin.forms demo apps to show the usage

---
+++ Demo/Demo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Demo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var content = new ContentPage {
+                Title = "Demo",
+                Content = new StackLayout {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Xamarin.Piwik Tracker Demo App"
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
